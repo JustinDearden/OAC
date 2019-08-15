1fc7f1a6210cefae798b233998af46bd08702d74
Justin Dearden <deardenj@uwindsor.ca>
2019-08-14 22:39:43 -0400

    Explore Back Code

---
+++ oac/oac/explore.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace oac
+{
+    public partial class explore : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["oacConnectionString"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                
+            }
+        }
+    }
+}
