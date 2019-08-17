fa2fd025ca91c4d0b66af89141189069f063edb9
Justin Dearden <deardenj@uwindsor.ca>
2019-08-16 20:04:22 -0400

    Add files via upload

---
+++ oac/oac/a_explore.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace oac
+{
+    public partial class a_explore : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["username"] == null)
+            {
+                Response.Redirect("index.aspx");
+            }
+
+            lblUserName.Text = "" + Session["username"];
+
+            string CS = ConfigurationManager.ConnectionStrings["oacConnectionString"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                if (!IsPostBack)
+                {
+
+                }
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("index.aspx");
+        }
+
+        protected void LinkButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("dashboard.aspx");
+        }
+    }
+}
